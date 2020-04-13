cefab75d8390d21fbcdfdf1b1c59ed3feba10f47
bellbind <bellbind@gmail.com>
2020-04-13 16:06:19 +0900

    [update] follow js-ipfs-042.0

--- test/cat-gif/main.js
+++ test/cat-gif/main.js
@@ -1,13 +1,14 @@
 import * as FetchIpfs from "http://localhost:10000/fetch-ipfs.js";
 
 //NOTE: matched versions for current Ipfs-0.40.0 and IpfsHttpResponse-0.4.0
-import "https://cdn.jsdelivr.net/npm/ipfs@0.40.0/dist/index.js";
-import "https://cdn.jsdelivr.net/npm/ipfs-http-response@0.4.0/dist/index.js";
+import "https://cdn.jsdelivr.net/npm/ipfs/dist/index.js";
+//import "https://cdn.jsdelivr.net/npm/ipfs@0.40.0/dist/index.js";
+//import "https://cdn.jsdelivr.net/npm/ipfs-http-response@0.4.0/dist/index.js";
 //console.debug(window.Ipfs);
 
 const main = async () => {
   console.info("[INFO] IPFS node spawn several logs includes WebSocket Errors");
-  const node = new Ipfs({
+  const node = await Ipfs.create({
     repo: `ipfs-${Math.random()}`,
     relay: {enabled: true, hop: {enabled: true, active: true}},
   });
@@ -16,8 +17,8 @@ const main = async () => {
   //console.debug("IPFS version:", (await node.version()).version);
   //console.debug(`Peer ID:`, (await node.id()).id);
 
-  //const ipfsFetch = FetchIpfs.createFetch(node);
-  const ipfsFetch = FetchIpfs.createFetch(node, {IpfsHttpResponse});
+  const ipfsFetch = FetchIpfs.createFetch(node);
+  //const ipfsFetch = FetchIpfs.createFetch(node, {IpfsHttpResponse});
   
   const url1 = "https://gateway.ipfs.io/ipfs/QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF/cat.gif";
   const res1 = await ipfsFetch(url1);
